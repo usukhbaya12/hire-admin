d0d1ef95a5d15bf8cb9ce8eff14756f8935d8c99
dorjoo <dorjoohover@gmail.com>
2026-02-20 11:59:04 +0800

    huuchin ruu shiljuulsen

--- utils/routes.js
+++ utils/routes.js
@@ -1,4 +1,3 @@
-export const api =
-  process.env.NEXT_PUBLIC_API_URL || "http://89.116.228.100/api/v1/";
+export const api = "https://srv666826.hstgr.cloud/api/v1/";
 // export const api = "http://localhost:3000/api/v1/";
 // export const api = "http://localhost:5000/api/v1/";
