b7d2cf8561c44ddadeff05f9a627f2169f09bf5b
dorjoo <dorjoohover@gmail.com>
2026-02-20 08:18:30 +0800

    || core iin url g solison

--- utils/routes.js
+++ utils/routes.js
@@ -1,3 +1,4 @@
-export const api = "https://srv666826.hstgr.cloud/api/v1/";
+export const api =
+  process.env.NEXT_PUBLIC_API_URL || "http://89.116.228.100/api/v1/";
 // export const api = "http://localhost:3000/api/v1/";
 // export const api = "http://localhost:5000/api/v1/";
