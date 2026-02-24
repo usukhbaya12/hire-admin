37ad5ddcdf95a1e10c298ea9eb62168b98d27c69
dorjoo <dorjoohover@gmail.com>
2026-02-25 03:22:17 +0800

    changed ip to dns

--- utils/routes.js
+++ utils/routes.js
@@ -1,3 +1,4 @@
-export const api = "http://76.13.211.121/api/v1/";
+// export const api = "http://76.13.211.121/api/v1/";
+export const api = 'https://api.hire.mn/api/v1/';
 // export const api = "http://localhost:3000/api/v1/";
 // export const api = "http://localhost:5000/api/v1/";
